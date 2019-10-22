a662d1495743fc7b5c85c7d2582a2b1f41bd79ba
Xingcan LAN <thynson@live.com>
2019-10-22 20:29:47 +0800

    Initial mikro-orm example

---
+++ examples/mikro-orm/src/index.ts
@@ -0,0 +1,62 @@
+import 'reflect-metadata';
+import {Body, Controller, GET, HttpModule, Path, POST, Query} from '@sensejs/http';
+import {EntryPoint} from '@sensejs/core';
+import {InjectRepository, MikroOrmModule, SenseHttpInterceptor} from '@sensejs/mikro-orm';
+import {EntityRepository} from 'mikro-orm';
+import {Author} from './entities/author';
+import {Book} from './entities/book';
+
+@Controller('/example')
+class ExampleHttpController {
+
+  constructor(@InjectRepository(Book) private bookRepository: EntityRepository<Book>,
+              @InjectRepository(Author) private authorRepository: EntityRepository<Author>) {
+  }
+
+  @GET('/')
+  handleGetRequest(@Query() query: object) {
+    return {
+      query,
+      timestamp: Date.now(),
+    };
+  }
+
+  @POST('/author')
+  async createAuthor(@Body() body: { name: string }) {
+    const author = new Author(body.name as string);
+    await this.bookRepository.persistAndFlush(author);
+    return author;
+  }
+
+  @POST('/author/:id/book')
+  async createBook(@Body() body: { name: string }, @Path('id') id: string) {
+
+    const author = await this.authorRepository.findOneOrFail(id);
+    const book = author.writeBook(body.name);
+    await this.bookRepository.persist(book);
+    await this.authorRepository.persistAndFlush(author);
+    return book;
+  }
+
+}
+
+const mikroOrmModule = MikroOrmModule({
+  dbName: 'test-mikro-orm',
+  entitiesDirs: ['./entities'],
+  baseDir: __dirname,
+  entities: [Book, Author],
+  entitiesDirsTs: ['../../src/mikro-orm/entities'],
+});
+
+// @EntryPoint()
+class App extends HttpModule({
+  requires: [mikroOrmModule],
+  httpOption: {
+    listenPort: 3000,
+    listenAddress: '0.0.0.0',
+  },
+  globalInterceptors: [SenseHttpInterceptor],
+  components: [ExampleHttpController],
+}) {
+
+}
--- packages/mikro-orm/src/index.ts
+++ packages/mikro-orm/src/index.ts
@@ -1,77 +1,118 @@
-import {Abstract, Constructor, Module, ModuleOption, setModuleMetadata} from '@sensejs/core';
-import {MikroORM} from 'mikro-orm';
-import {inject, AsyncContainerModule, Container} from 'inversify';
+import {Component, Constructor, Module, ModuleOption, RequestContext, RequestInterceptor, ModuleConstructor} from '@sensejs/core';
+import {EntityManager, MikroORM} from 'mikro-orm';
+import {AsyncContainerModule, Container, inject} from 'inversify';
 
 interface MikroOrmModuleOption extends ModuleOption {
-    entities: Constructor<unknown>[],
+  entities: Constructor<unknown>[];
+  dbName: string;
+  baseDir: string;
+  entitiesDirs: string[];
+  entitiesDirsTs: string[];
 }
 
 const EntityRepositoryMetadataKey = Symbol();
 
-function ensureInjectRepositoryToken(entityConstructor: Constructor<unknown>) {
-    let symbol = Reflect.get(entityConstructor, EntityRepositoryMetadataKey);
-    if (symbol) {
-        return symbol;
-    }
-    symbol = Symbol();
-    Reflect.set(entityConstructor, EntityRepositoryMetadataKey, Symbol());
+function ensureInjectRepositoryToken(entityConstructor: Constructor<unknown>): symbol {
+  let symbol = Reflect.get(entityConstructor, EntityRepositoryMetadataKey);
+  if (symbol) {
     return symbol;
+  }
+  symbol = Symbol(entityConstructor.name);
+  Reflect.set(entityConstructor, EntityRepositoryMetadataKey, symbol);
+  return symbol;
 }
 
-function getInjectRepositoryToken(entityConstructor: Constructor<unknown>): symbol|undefined {
-    return Reflect.get(entityConstructor, EntityRepositoryMetadataKey);
+function getInjectRepositoryToken(entityConstructor: Constructor<unknown>): symbol | undefined {
+  return Reflect.get(entityConstructor, EntityRepositoryMetadataKey);
 }
 
 export function InjectRepository(entityConstructor: Constructor<unknown>) {
-    const symbol = ensureInjectRepositoryToken(entityConstructor);
-    return inject(symbol);
+  const symbol = ensureInjectRepositoryToken(entityConstructor);
+  return inject(symbol);
 }
 
+export class RepositoryRegister {
 
+  constructor(private entityManager: EntityManager, private entities: Constructor<unknown>[]) {
 
-export function MikroOrmModule(option: MikroOrmModuleOption) {
+  }
 
-    return (class extends Module(option) {
+  registerOnContainer(container: Container) {
+    const forkedEntityManager = this.entityManager.fork();
+    container.bind(EntityManager).toConstantValue(forkedEntityManager);
+    for (const entityClass of this.entities) {
+      container.bind<unknown>(ensureInjectRepositoryToken(entityClass))
+        .toConstantValue(forkedEntityManager.getRepository(entityClass));
+    }
 
-        ormInstance?: MikroORM;
-        ormModule?: AsyncContainerModule;
+  }
 
-        async onCreate(container: Container): Promise<void> {
-            super.onCreate(container);
-            this.ormModule = new AsyncContainerModule(async (bind) => {
-                const ormInstance = await MikroORM.init({
-                    entities: option.entities,
-                    dbName: 'test',
-                    baseDir: __dirname,
-                    entitiesDirs: [],
-                    entitiesDirsTs:[]
-                });
-                this.ormInstance = ormInstance;
-                option.entities.forEach((entityConstructor) => {
-                    const repositoryInjectToken = ensureInjectRepositoryToken(entityConstructor);
-                    bind(repositoryInjectToken).toDynamicValue(()=> {
-                        return ormInstance.em.getRepository(entityConstructor);
-                    }).inRequestScope();
+  registerOnHttpInterceptor(httpContext: RequestContext) {
+    const forkedEntityManager = this.entityManager.fork();
+    httpContext.bindContextValue(EntityManager, forkedEntityManager);
+    for (const entityClass of this.entities) {
+      const symbol = ensureInjectRepositoryToken(entityClass);
+      httpContext.bindContextValue(symbol, forkedEntityManager.getRepository(entityClass));
+    }
+  }
+}
 
-                });
-            });
-            return container.loadAsync(this.ormModule);
-        }
+@Component()
+export class SenseHttpInterceptor extends RequestInterceptor {
 
-        async onDestroy(container: Container): Promise<void> {
-            if (this.ormModule) {
-                container.unload(this.ormModule);
-                this.ormModule = undefined;
-            }
-            if (this.ormInstance) {
-                await this.ormInstance.close();
-                this.ormInstance = undefined;
-            }
-            return super.onDestroy(container);
-        }
-    });
+  constructor(
+    @inject(RepositoryRegister) private repositoryRegister: RepositoryRegister,
+  ) {
+    super();
+  }
+
+  async intercept(context: RequestContext, next: () => Promise<unknown>) {
+    this.repositoryRegister.registerOnHttpInterceptor(context);
+    await next();
+  }
 
 }
 
+export function MikroOrmModule(option: MikroOrmModuleOption): ModuleConstructor {
 
+  class MikroOrmModule extends Module(option) {
 
+    ormInstance?: MikroORM;
+    ormModule?: AsyncContainerModule;
+
+    constructor(@inject(Container) private container: Container) {
+      super();
+    }
+
+    async onCreate(): Promise<void> {
+      // super.onCreate(container);
+      this.ormModule = new AsyncContainerModule(async (bind) => {
+        const ormInstance = await MikroORM.init({
+          entities: option.entities,
+          dbName: option.dbName,
+          baseDir: option.baseDir,
+          entitiesDirs: option.entitiesDirs,
+          entitiesDirsTs: option.entitiesDirsTs,
+        });
+        this.ormInstance = ormInstance;
+        bind(RepositoryRegister).toConstantValue(new RepositoryRegister(ormInstance.em, option.entities));
+      });
+      return this.container.loadAsync(this.ormModule);
+    }
+
+    async onDestroy(): Promise<void> {
+      if (this.ormModule) {
+        this.container.unload(this.ormModule);
+        this.ormModule = undefined;
+      }
+      if (this.ormInstance) {
+        await this.ormInstance.close();
+        this.ormInstance = undefined;
+      }
+      return super.onDestroy();
+    }
+  }
+
+  return MikroOrmModule;
+
+}
