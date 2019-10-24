9c3984412f208e53be65069cfb46fed291f7fd09
Xingcan LAN <thynson@live.com>
2019-10-24 18:54:51 +0800

    Move jest configuration out from package.json

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    collectCoverageFrom: [ "packages/*/src/**/*.ts" ],
+    testRegex: "./packages/[^/]+/tests/.+\\.ts$",
+    testEnvironment: "node",
+    transform: {
+        "^.+\\.tsx?$": "ts-jest"
+    },
+    coveragePathIgnorePatterns: [
+        "/node_modules/",
+        "/packages/*/lib"
+    ],
+    modulePathIgnorePatterns: [
+        "/test/fixtures/",
+        "/test/tmp/",
+        "/test/__data__/",
+        "<rootDir>/build/"
+    ],
+    moduleNameMapper: {
+        "^@sensejs/([a-zA-Z0-9_-]+)$": "<rootDir>/packages/$1/"
+    }
+};
