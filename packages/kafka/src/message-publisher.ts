9ae94c119a934ed29f5e84530dfea592b723b4a3
Xingcan LAN <thynson@live.com>
2019-11-12 16:44:07 +0800

    Rewrite producer and its test

--- packages/kafka/src/message-publisher.ts
+++ packages/kafka/src/message-publisher.ts
@@ -1,5 +1,5 @@
 import {CustomPartitioner, HighLevelProducer, KafkaClient, ProduceRequest, ProducerOptions} from 'kafka-node';
-import {promisify} from 'util';
+import {Writable} from 'stream';
 
 export interface ProducerOption {
   kafkaHost: string;
@@ -11,59 +11,23 @@ export interface ProducerOption {
 
 type MessageContent = string | Buffer;
 
-interface SendRequest {
-  topic: string;
-  messages: MessageContent;
-  done: (e?: Error) => unknown;
-  key?: MessageContent;
-}
-
-export class Producer {
-
-  private readonly _createTopics: string[];
+export class MessageProducer {
   private readonly _option: ProducerOption;
   private readonly _maxMessageSize: number;
   private readonly _maxMessageBatchSize: number;
-  private readonly _bufferedMessages: SendRequest[];
-  private _sendMessagePromise?: Promise<unknown>;
-  private _initialized?: Promise<HighLevelProducer>;
-  private _refreshMetadataPromise?: Promise<HighLevelProducer>;
-  private _heartBeatTimer?: NodeJS.Timer;
+  private _initialized?: Promise<Writable>;
+  private _highLevelProducer?: HighLevelProducer;
 
   /**
    * Constructor
    * @param option
    * @param option.kafkaHost {String}
    * @param [option.producerOptions]
-   * @param createTopics {String | Array<String>}
    */
-  constructor(option: ProducerOption, createTopics: string | string[] = []) {
-
-    if (typeof createTopics === 'string') {
-      if (createTopics === '') {
-        throw new TypeError('param "createTopics" should not be a empty string.');
-      }
-      this._createTopics = [createTopics];
-    } else if (!Array.isArray(createTopics)) {
-      throw new TypeError('param "createTopics" should be a non-empty string or an array of non-empty string');
-    } else if (createTopics.filter((topic) =>  topic !== '').length > 0) {
-      throw new TypeError('array param "createTopics" contains element of non-string object or empty string');
-    } else {
-      this._createTopics = createTopics;
-    }
-
+  constructor(option: ProducerOption) {
     this._option = option;
     this._maxMessageSize = option.maxMessageSize || 65536;
     this._maxMessageBatchSize = option.maxMessageBatchSize || 16;
-    this._bufferedMessages = [];
-  }
-
-  async initialize(): Promise<this> {
-    if (!this._initialized) {
-      this._initialized = this._doInitialization();
-    }
-    await this._initialized;
-    return this;
   }
 
   /**
@@ -74,35 +38,58 @@ export class Producer {
    * @returns {Promise}
    */
   async sendMessage(topic: string, messages: MessageContent, key?: MessageContent) {
-    await this.initialize();
-    this._checkMessageSize(messages);
-    return this._internalSendMessage(topic, messages, key);
+    if (!this._initialized) {
+      throw new Error('not connected or closed');
+    }
+    const stream = await this._initialized;
+    const chunk: ProduceRequest = {
+      topic,
+      messages,
+      key,
+    };
+    return new Promise((resolve, reject) => stream.write(chunk, (e) => (e ? reject(e) : resolve())));
   }
 
   async close() {
-    if (!this._initialized) {
+    const initialized = this._initialized;
+    delete this._initialized;
+    if (!initialized) {
       return;
     }
-    const producer = await this._initialized;
-    this._clearRefreshMetadataTimer();
-    if (producer) {
-      // @ts-ignore
-      return Bluebird.fromCallback((callback) => producer.close(callback));
-    }
+
+    return initialized
+      .then((writable) => {
+        return new Promise((resolve, reject) => {
+          writable.once('error', reject);
+          writable.end(() => resolve(writable));
+        });
+      })
+      .then(() => {
+        const highLevelProducer = this._highLevelProducer;
+        delete this._highLevelProducer;
+        if (highLevelProducer) {
+          return new Promise((resolve, reject) => highLevelProducer.close((e?) => (e ? reject(e) : resolve())));
+        }
+      });
   }
 
-  /**
-   * Initialize the producer
-   * @returns {Promise<HighLevelProducer>}
-   * @private
-   */
-  private _doInitialization() {
-    this._initialized = new Promise<HighLevelProducer>((done, fail) => {
+  public async initialize() {
+    await this._doInitialize();
+    return this;
+  }
+
+  private _doInitialize() {
+    if (this._initialized) {
+      return this._initialized;
+    }
+    return (this._initialized = new Promise<HighLevelProducer>((done, fail) => {
       const client = new KafkaClient({kafkaHost: this._option.kafkaHost});
 
-      const producer = new HighLevelProducer(client,
+      const producer = new HighLevelProducer(
+        client,
         Object.assign({requireAcks: 1, ackTimeoutMs: 1000}, this._option.producerOptions),
-        this._option.customPartition);
+        this._option.customPartition,
+      );
 
       const errorBeforeReady = (error: Error) => {
         return client.close(() => fail(error));
@@ -111,111 +98,21 @@ export class Producer {
       producer.once('error', errorBeforeReady);
       producer.once('ready', () => {
         producer.removeListener('error', errorBeforeReady);
-        producer.createTopics(this._createTopics, true, (e) => {
-          if (e) {
-            return fail(e);
-          }
-          return done(producer);
-        });
+        done(producer);
       });
-    });
-    this._initialized.then((producer) => {
-      // @ts-ignore
-      const refreshMetadataInterval = producer?.client?.options.idleConnection;
-      if (typeof refreshMetadataInterval === 'number' && refreshMetadataInterval === 0) {
-        this._heartBeatTimer = setInterval(async () => {
-          // @ts-ignore send empty metadata request to keep connection alive, do it only when no
-          producer.client.loadMetadataForTopics([], (e) => {
-          });
-        }, refreshMetadataInterval / 2);
-      }
-    });
-    return this._initialized;
-  }
-
-  private _clearRefreshMetadataTimer() {
-    if (this._heartBeatTimer) {
-      clearInterval(this._heartBeatTimer);
-      delete this._heartBeatTimer;
-    }
-  }
-
-  private _refreshMetadata() {
-    if (this._refreshMetadataPromise) {
-      return this._refreshMetadataPromise;
-    }
-    this._refreshMetadataPromise = this._doInitialization()
-      .then((producer) => {
-        return promisify((callback) => {
-          // @ts-ignore
-          return producer.client.refreshMetadata(this._createTopics, callback);
-        })()
-          .then(() => {
-            // @ts-ignore
-            producer.client.once('brokersChanged', () => {
-              delete this._refreshMetadataPromise;
-            });
-            return producer;
-          });
+    }).then((highLevelProducer: HighLevelProducer) => {
+      this._highLevelProducer = highLevelProducer;
+      return new Writable({
+        objectMode: true,
+        decodeStrings: false,
+        highWaterMark: 16,
+        write: (chunk: ProduceRequest, encoding, callback) => {
+          highLevelProducer.send([chunk], callback);
+        },
+        writev: (chunks: {chunk: ProduceRequest}[], callback) => {
+          highLevelProducer.send(chunks.map((x) => x.chunk), callback);
+        },
       });
-    return this._refreshMetadataPromise;
-  }
-
-  private async _sendBufferedMessage(bufferedMessages: SendRequest[]) {
-    let error: Error;
-    try {
-      const producer = await this._refreshMetadata();
-      await (promisify((callback) => {
-        return producer.send(bufferedMessages.map(({topic, key, messages}): ProduceRequest => {
-          return {topic, key, messages};
-        }), callback);
-      })());
-    } catch (e) {
-      error = e;
-    } finally {
-      bufferedMessages.forEach(({done}) => done(error));
-    }
-  }
-
-  private async _sendBufferedMessageUntilEmpty() {
-    try {
-      while (this._bufferedMessages.length > 0) {
-        const batchMessage = this._bufferedMessages.splice(0, this._maxMessageBatchSize);
-        this._sendMessagePromise = this._sendBufferedMessage(batchMessage);
-        await this._sendMessagePromise;
-      }
-    } finally {
-      delete this._sendMessagePromise;
-    }
-  }
-
-  private _checkMessageSize(messages: MessageContent): void {
-    if (typeof messages === 'string') {
-      messages = Buffer.from(messages, 'utf-8');
-    }
-    if (messages.length >= this._maxMessageSize) {
-      throw new Error(`Message size exceeded limit of ${this._maxMessageSize}`);
-    }
-  }
-
-  private _internalSendMessage(topic: string,
-                               messages: MessageContent,
-                               key?: MessageContent) {
-    const sendPromise = new Promise<void>((resolve, reject) => {
-      this._bufferedMessages.push({
-        topic, messages, key, done: (e?) => {
-          if (e) {
-            return reject(e);
-          }
-          return resolve();
-        }
-      });
-    });
-
-    if (!this._sendMessagePromise) {
-      this._sendBufferedMessageUntilEmpty();
-    }
-
-    return sendPromise;
+    }));
   }
 }
---
+++ packages/kafka/src/publish-module.ts
@@ -0,0 +1,56 @@
+import {Component, ComponentFactory, ComponentScope, Module, ModuleConstructor} from '@sensejs/core';
+import {MessageProducer, ProducerOption} from './message-publisher';
+import {inject} from 'inversify';
+
+export interface KafkaPublishModuleOption {
+  kafkaProducerOption: ProducerOption;
+}
+
+export function KafkaPublishModule(option: KafkaPublishModuleOption): ModuleConstructor {
+  @Component()
+  class KafkaProducerConnectionFactory extends ComponentFactory<MessageProducer> {
+    private messageProducer?: MessageProducer;
+
+    build(): MessageProducer {
+      if (this.messageProducer) {
+        return this.messageProducer;
+      }
+      throw new Error('messageProducer not connected yet');
+    }
+
+    async connect(option: ProducerOption): Promise<MessageProducer> {
+      const messageProducer = new MessageProducer(option);
+      await messageProducer.initialize();
+      this.messageProducer = messageProducer;
+      return this.messageProducer;
+    }
+
+    async close() {
+      if (this.messageProducer) {
+        const messageProducer = this.messageProducer;
+        delete this.messageProducer;
+        await messageProducer.close();
+      }
+    }
+  }
+
+  class KafkaPublishModule extends Module({
+    factories: [{provide: MessageProducer, factory: KafkaProducerConnectionFactory, scope: ComponentScope.SINGLETON}],
+  }) {
+    constructor(@inject(KafkaProducerConnectionFactory) private factory: KafkaProducerConnectionFactory) {
+      super();
+    }
+
+    async onCreate(): Promise<void> {
+      super.onCreate();
+      await this.factory.connect(option.kafkaProducerOption);
+    }
+
+    async onDestroy(): Promise<void> {
+      await this.factory.close();
+      return super.onDestroy();
+    }
+  }
+
+  return KafkaPublishModule;
+}
