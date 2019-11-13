206d514d75468e585261f119f6d3537cc8830f13
Nathan <308719298@qq.com>
2019-11-13 15:15:32 +0800

    format: pretty code

--- packages/redis/tests/redis-module.spec.ts
+++ packages/redis/tests/redis-module.spec.ts
@@ -7,56 +7,46 @@ import {RedisModule} from '../src';
 const DEFAULT_VALUE = 'test';
 
 describe('RedisModule', () => {
+  test('common case', async () => {
+    @Controller('/example')
+    class ExampleHttpController {
+      constructor(@inject(Redis) private redisClient: Redis.Redis) {}
 
-    test('common case', async () => {
+      @GET('/get')
+      async getDefaultValue() {
+        const key = 'default_value';
+        await this.redisClient.set(key, DEFAULT_VALUE);
+        const value = await this.redisClient.get(key);
+        return value;
+      }
+    }
 
-        @Controller('/example')
-        class ExampleHttpController {
-
-            constructor(@inject(Redis) private redisClient: Redis.Redis
-            ) {
-            }
-
-            @GET('/get')
-            async getDefaultValue() {
-                const key = 'default_value';
-                await this.redisClient.set(key, DEFAULT_VALUE);
-                const value = await this.redisClient.get(key);
-                return value;
-            }
-
-        }
-
-        const redisModule = RedisModule({
-            uri: ''
-        });
-
-        const spy = jest.fn();
-
-        class FooModule extends Module({components: [ExampleHttpController], requires: [redisModule]}) {
-            constructor(@inject(Container) private container: Container) {
-                super();
-            }
-
-            async onCreate() {
-                const controller = this.container.get(ExampleHttpController);
-                const result = await controller.getDefaultValue();
-                expect(result).toEqual(DEFAULT_VALUE);
-                spy();
-            }
-
-            async onDestroy() {
-
-            }
-        }
-
-        try {
-            const app = new ApplicationFactory(FooModule);
-            await app.start();
-            await app.stop();
-            expect(spy).toBeCalled();
-        } catch (e) {}
-
+    const redisModule = RedisModule({
+      uri: '',
     });
 
+    const spy = jest.fn();
+
+    class FooModule extends Module({components: [ExampleHttpController], requires: [redisModule]}) {
+      constructor(@inject(Container) private container: Container) {
+        super();
+      }
+
+      async onCreate() {
+        const controller = this.container.get(ExampleHttpController);
+        const result = await controller.getDefaultValue();
+        expect(result).toEqual(DEFAULT_VALUE);
+        spy();
+      }
+
+      async onDestroy() {}
+    }
+
+    try {
+      const app = new ApplicationFactory(FooModule);
+      await app.start();
+      await app.stop();
+      expect(spy).toBeCalled();
+    } catch (e) {}
+  });
 });
