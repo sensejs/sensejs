ba249efd567fdebb1b5d998763047cbd69ee74c4
Nathan <308719298@qq.com>
2019-11-13 15:15:32 +0800

    format: pretty code

--- packages/redis/__mocks__/ioredis.ts
+++ packages/redis/__mocks__/ioredis.ts
@@ -1,36 +1,35 @@
-import { EventEmitter } from 'events';
+import {EventEmitter} from 'events';
 
 class MockRedis extends EventEmitter {
-    private map: Map<string, any>;
-    private status: string;
+  private map: Map<string, any>;
+  private status: string;
 
-    constructor() {
-        super();
-        this.map = new Map();
-        this.status = 'disconnect';
-    }
+  constructor() {
+    super();
+    this.map = new Map();
+    this.status = 'disconnect';
+  }
 
-    once(event: string, listener: (...args: any[]) => void) {
-        if (event === 'error') {
-            throw new Error('Redis connect has some error');
-        } else if (event === 'connect' && this.status === 'disconnect') {
-            this.status = 'connect';
-            return this;
-        }
-        return this;
+  once(event: string, listener: (...args: any[]) => void) {
+    if (event === 'error') {
+      throw new Error('Redis connect has some error');
+    } else if (event === 'connect' && this.status === 'disconnect') {
+      this.status = 'connect';
+      return this;
     }
+    return this;
+  }
 
-    async disconnect() {
-    }
+  async disconnect() {}
 
-    async set(key: string, value: any) {
-        this.map.set(key, value);
-        return key;
-    }
+  async set(key: string, value: any) {
+    this.map.set(key, value);
+    return key;
+  }
 
-    async get(key: string) {
-        return this.map.get(key);
-    }
+  async get(key: string) {
+    return this.map.get(key);
+  }
 }
 
 export = MockRedis;
