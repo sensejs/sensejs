79f78ce090910fc415fdeedfee598c762f3d7b38
LAN Xingcan <thynson@live.com>
2019-11-23 19:36:22 +0800

    Add tracing in example

--- packages/create-app/src/http/index.ts
+++ packages/create-app/src/http/index.ts
@@ -4,6 +4,7 @@ import {ExampleController} from './example.controller';
 import {RequestTimingInterceptor} from './request-timing.interceptor';
 import PublishingModule from '../example';
 import logger from '@sensejs/logger';
+import {TracingInterceptor} from './tracing-interceptor';
 
 export default class ExampleHttpModule extends HttpModule({
   httpOption: {
@@ -12,7 +13,7 @@ export default class ExampleHttpModule extends HttpModule({
   },
   requires: [PublishingModule],
   components: [ExampleController, RequestTimingInterceptor],
-  globalInterceptors: [RequestTimingInterceptor],
+  globalInterceptors: [TracingInterceptor, RequestTimingInterceptor],
   injectOptionFrom: 'config.http',
 }) {
   constructor(@InjectLogger(ExampleHttpModule) private logger: Logger) {
---
+++ packages/create-app/src/http/tracing-interceptor.ts
@@ -0,0 +1,16 @@
+import {Component, InjectLogger, Logger, LoggerFactory} from '@sensejs/core';
+import {HttpInterceptor, HttpContext} from '@sensejs/http';
+import {defaultLoggerBuilder} from '@sensejs/logger';
+import uuid from 'uuid/v4';
+
+@Component()
+export class TracingInterceptor extends HttpInterceptor {
+  constructor(@InjectLogger(TracingInterceptor) private logger: Logger) {
+    super();
+  }
+
+  async intercept(context: HttpContext, next: () => Promise<void>): Promise<void> {
+    context.bindContextValue(LoggerFactory, new LoggerFactory(defaultLoggerBuilder.setTraceId(uuid())));
+    return next();
+  }
+}
