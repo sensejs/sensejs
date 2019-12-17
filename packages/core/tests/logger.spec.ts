4185dd0062e6a5510771f49d1858f2bd77e47492
LAN Xingcan <thynson@live.com>
2019-12-18 03:14:13 +0800

    Correctly mock when testing logger

--- packages/core/tests/logger.spec.ts
+++ packages/core/tests/logger.spec.ts
@@ -10,12 +10,14 @@ import {
   ModuleRoot,
 } from '../src';
 
-jest.spyOn(global.console, 'error').mockImplementation(jest.fn());
-jest.spyOn(global.console, 'warn').mockImplementation(jest.fn());
-jest.spyOn(global.console, 'info').mockImplementation(jest.fn());
-jest.spyOn(global.console, 'log').mockImplementation(jest.fn());
-jest.spyOn(global.console, 'debug').mockImplementation(jest.fn());
-jest.spyOn(global.console, 'trace').mockImplementation(jest.fn());
+beforeEach(() => {
+  jest.spyOn(global.console, 'error').mockImplementation(jest.fn());
+  jest.spyOn(global.console, 'warn').mockImplementation(jest.fn());
+  jest.spyOn(global.console, 'info').mockImplementation(jest.fn());
+  jest.spyOn(global.console, 'log').mockImplementation(jest.fn());
+  jest.spyOn(global.console, 'debug').mockImplementation(jest.fn());
+  jest.spyOn(global.console, 'trace').mockImplementation(jest.fn());
+});
 
 test('ConsoleLogger', () => {
   const logger = new ConsoleLoggerBuilder().build();
