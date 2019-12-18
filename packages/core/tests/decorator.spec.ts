28a03bf1ab947415fea4eb2c0f4b295f07d57c59
LAN Xingcan <thynson@live.com>
2019-12-19 02:09:12 +0800

    Add test case for Optional

--- packages/core/tests/decorator.spec.ts
+++ packages/core/tests/decorator.spec.ts
@@ -1,5 +1,60 @@
-import {Component, getComponentMetadata, Inject, Named, Tagged, Module, ModuleRoot, invokeMethod} from '../src';
+import {
+  Component,
+  getComponentMetadata,
+  Inject,
+  Named,
+  Tagged,
+  Module,
+  ModuleRoot,
+  invokeMethod,
+  Optional,
+} from '../src';
 import {Container} from 'inversify';
+
+describe('Optional', () => {
+  const injectToken = Symbol();
+  test('Decorate constructor param', async () => {
+
+    const stub = jest.fn();
+    @Component()
+    class X {
+      constructor(@Inject(injectToken) @Optional() param: any) {
+        stub(param);
+      }
+    }
+
+    class MyModule extends Module({components: [X]}) {
+      constructor(@Inject(X) x: any) {
+        super();
+      }
+    }
+
+    await new ModuleRoot(MyModule).start();
+    expect(stub).toHaveBeenCalledWith(undefined);
+  });
+
+  test('Optional method inject', async () => {
+    const stub = jest.fn();
+    @Component()
+    class X {
+      constructor(@Inject(injectToken) @Optional() param: any) {
+      }
+      method(@Inject(injectToken) @Optional() param: any) {
+        stub(param);
+      }
+    }
+
+    class MyModule extends Module({components: [X]}) {
+      constructor(@Inject(Container) container: any) {
+        super();
+        invokeMethod(container, X, X.prototype.method);
+      }
+    }
+
+    await new ModuleRoot(MyModule).start();
+    expect(stub).toHaveBeenCalledWith(undefined);
+  });
+});
 
 describe('Named', () => {
 
@@ -9,11 +64,13 @@ describe('Named', () => {
 
   @Component({id})
   @Named(name1)
-  class MyComponent1 {}
+  class MyComponent1 {
+  }
 
   @Component({id})
   @Named(name2)
-  class MyComponent2 {}
+  class MyComponent2 {
+  }
 
   test('Named component', () => {
     expect(getComponentMetadata(MyComponent1)).toEqual(expect.objectContaining({name: name1}));
@@ -22,13 +79,15 @@ describe('Named', () => {
 
   test('Method inject', () => {
     const result = Math.random();
+
     @Component()
     class X {
       callable(@Inject(id) @Named(name1) foo1: MyComponent1, @Inject(id) @Named(name2) foo2: MyComponent2) {
         return result;
       }
 
-      nonCallable(@Inject(id) @Named(name1) foo1: MyComponent1, @Inject(id) @Named(name2) foo2: MyComponent2) {}
+      nonCallable(@Inject(id) @Named(name1) foo1: MyComponent1, @Inject(id) @Named(name2) foo2: MyComponent2) {
+      }
     }
 
     class MyModule extends Module({components: [MyComponent1, MyComponent2, X]}) {
@@ -46,15 +105,18 @@ describe('Named', () => {
     class Resolvable {
       constructor(
         @Inject(id) @Named(name1) public foo1: MyComponent1,
-        @Inject(id) @Named(name2) public foo2: MyComponent2
-      ) {}
+        @Inject(id) @Named(name2) public foo2: MyComponent2,
+      ) {
+      }
     }
+
     @Component()
     class NonResolvable {
       constructor(
         @Inject(id) public foo1: MyComponent1,
-        @Inject(id) public foo2: MyComponent2
-      ) {}
+        @Inject(id) public foo2: MyComponent2,
+      ) {
+      }
     }
 
     class MyModule extends Module({components: [MyComponent1, MyComponent2, Resolvable]}) {
@@ -88,7 +150,8 @@ describe('Decorators', () => {
     @Tagged(numberTagKey, numberTagValue)
     @Tagged(stringTagKey, stringTagValue)
     @Tagged(symbolTagKey, symbolTagValue)
-    class MyComponent {}
+    class MyComponent {
+    }
 
     const x = Tagged('key', 'value');
     x(MyComponent);
@@ -108,16 +171,20 @@ describe('Decorators', () => {
     const result = Math.random();
     const key = Symbol();
     const value = Symbol();
+
     @Tagged(key, value)
     @Component()
-    class Y {}
+    class Y {
+    }
+
     @Component()
     class X {
       callable(@Inject(Y) @Tagged(key, value) foo1: Y) {
         return result;
       }
 
-      nonCallable(@Inject(Y) foo2: Y) {}
+      nonCallable(@Inject(Y) foo2: Y) {
+      }
     }
 
     class MyModule extends Module({components: [X, Y]}) {
@@ -127,6 +194,7 @@ describe('Decorators', () => {
         expect(() => invokeMethod(container, X, X.prototype.nonCallable)).toThrow();
       }
     }
+
     await new ModuleRoot(MyModule).start();
   });
 
@@ -141,40 +209,46 @@ describe('Decorators', () => {
     @Component({id})
     @Tagged(key1, name1)
     // @Tagged(key2, name1)
-    class MyComponent1 {}
+    class MyComponent1 {
+    }
 
     @Component({id})
     @Tagged(key1, name2)
     // @Tagged(key2, name2)
-    class MyComponent2 {}
+    class MyComponent2 {
+    }
 
     @Component()
     class Resolvable1 {
       constructor(
         @Inject(id) @Tagged(key1, name1) public foo1: MyComponent1,
-        @Inject(id) @Tagged(key1, name2) public foo2: MyComponent2
-      ) {}
+        @Inject(id) @Tagged(key1, name2) public foo2: MyComponent2,
+      ) {
+      }
     }
 
     @Component()
     class NonResolvable1 {
       constructor(
         @Inject(id) public foo1: MyComponent1,
-        @Inject(id) public foo2: MyComponent2
-      ) {}
+        @Inject(id) public foo2: MyComponent2,
+      ) {
+      }
     }
 
     @Component()
     class NonResolvable2 {
       constructor(
         @Inject(id) @Tagged(key2, '') public foo1: MyComponent1,
-        @Inject(id) @Tagged(key2, '') public foo2: MyComponent2
-      ) {}
+        @Inject(id) @Tagged(key2, '') public foo2: MyComponent2,
+      ) {
+      }
     }
+
     class MyModule extends Module({
       components: [
-        MyComponent1, MyComponent2, Resolvable1, NonResolvable1, NonResolvable2
-      ]
+        MyComponent1, MyComponent2, Resolvable1, NonResolvable1, NonResolvable2,
+      ],
     }) {
 
       constructor(@Inject(Container) container: Container) {
@@ -194,6 +268,7 @@ describe('Decorators', () => {
         expect(() => container.get(NonResolvable2)).toThrow();
       }
     }
+
     await new ModuleRoot(MyModule).start();
   });
 });
