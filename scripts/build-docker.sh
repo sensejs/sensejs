a4cd4c18bb27a428cb1ddf8ef67717039af4e3df
LAN Xingcan <thynson@live.com>
2020-01-14 01:22:19 +0800

    chore(ci): run coveralls outside docker

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -8,7 +8,7 @@
 function baseImage() {
 
     echo "FROM node:${NODE_VERSION-lts} as base"
-    echo "RUN npm i -g pnpm typescript"
+    echo "RUN curl -L https://unpkg.com/@pnpm/self-installer | node"
     echo "WORKDIR /opt/sensejs"
     echo "ADD package.json pnpm-lock.yaml pnpm-workspace.yaml ./"
     for package in packages/* tools/* examples/*; do
@@ -25,14 +25,4 @@ function buildRoot() {
     echo "RUN pnpm run build"
 }
 
-#function copyAllPackages() {
-#    echo "FROM base AS target"
-#    echo "RUN pnpm recursive install --frozen-lockfile --only production"
-#    for package in packages/* tools/* examples/*; do
-#        if [[ -f ${package}/package.json ]]; then
-#            echo "COPY --from=dev /opt/sensejs/${package}/package.json /opt/sensejs/${package}/package.json"
-#            echo "COPY --from=dev /opt/sensejs/${package}/lib/ /opt/sensejs/${package}/lib/"
-#        fi
-#    done
-#}
 (baseImage; buildRoot) | docker build -f - -t sensejs . $@
