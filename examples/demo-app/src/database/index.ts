1cec3f107d698d2341f84cf78412ee6f6c9822d0
LAN Xingcan <thynson@live.com>
2020-01-28 22:21:37 +0800

    improvement(example): add subscriber to typeorm option

--- examples/demo-app/src/database/index.ts
+++ examples/demo-app/src/database/index.ts
@@ -4,6 +4,7 @@ export default createTypeOrmModule({
   typeOrmOption: {
     synchronize: true,
     entities: [__dirname + '/../**/*.entity.*'],
+    subscribers: [__dirname + '/../**/*.subscriber.*'],
     logging: true,
   },
   injectOptionFrom: 'config.database',
