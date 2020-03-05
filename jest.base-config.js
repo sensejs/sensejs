5b3c90a4d981507749730854cb0b6956798c28ff
LAN Xingcan <thynson@live.com>
2020-03-06 01:28:59 +0800

    fix: compilation error of test files was omitted on mac os
    This seems to be a bug of ts-jest. If ts-jest is not a devDependency
    of sub-package, jest will omit compile error of test files in such
    package. While ts-jest works correctly on linux.

--- jest.base-config.js
+++ jest.base-config.js
@@ -12,10 +12,6 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest',
   },
   coveragePathIgnorePatterns: ['/node_modules/', '/packages/*/lib'],
-  modulePathIgnorePatterns: ['/test/fixtures/', '/test/tmp/', '/test/__data__/', '<rootDir>/build/'],
-  moduleNameMapper: {
-    '^@sensejs/([a-zA-Z0-9_-]+)$': __dirname+'/packages/$1/',
-  },
   setupFiles: [__dirname+'/jest-setup.js'],
   resetMocks: true,
   restoreMocks: true,
@@ -23,6 +19,7 @@ module.exports = {
     'ts-jest': {
       packageJson: '<rootDir>/package.json',
       tsConfig: '<rootDir>/tests/tsconfig.json',
+      diagnostics: 'pretty'
     },
   },
 };
--- jest.config.js
+++ jest.config.js
@@ -1,29 +1,4 @@
 module.exports = {
   projects: ['packages/*/jest.config.js'],
+  ...require('./jest.base-config')
 };
-
-// const testRegex = performEndToEndTest
-//   ? './packages/[^/]+/tests/.+\\.(e2e-)?(test|spec).ts$'
-//   : './packages/[^/]+/tests/.+\\.(test|spec).ts$';
-//
-// module.exports = {
-//   collectCoverageFrom: ['packages/*/src/**/*.ts'],
-//   testRegex,
-//   testEnvironment: 'node',
-//   transform: {
-//     '^.+\\.tsx?$': 'ts-jest',
-//   },
-//   coveragePathIgnorePatterns: ['/node_modules/', '/packages/*/lib'],
-//   modulePathIgnorePatterns: ['/test/fixtures/', '/test/tmp/', '/test/__data__/', '<rootDir>/build/'],
-//   moduleNameMapper: {
-//     '^@sensejs/([a-zA-Z0-9_-]+)$': '<rootDir>/packages/$1/',
-//   },
-//   setupFiles: ['./jest-setup.js'],
-//   resetMocks: true,
-//   restoreMocks: true,
-//   globals: {
-//     'ts-jest': {
-//       tsConfig: 'tsconfig.test.json',
-//     },
-//   },
-// };
