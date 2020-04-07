f32c0c5e96e0c57a0314502e4789ccc8baec681b
LAN Xingcan <thynson@live.com>
2020-04-07 17:11:11 +0800

    chore: try to update release script.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,5 +7,4 @@ pnpm recursive run cleanup
 pnpm recursive run prepare
 PNPM_RECURSIVE='pnpm recursive --workspace-concurrency=1 --filter ./packages'
 $PNPM_RECURSIVE exec -- pnpm version --no-git-tag-version $1
-ALL_VERSIONS=`pnpm recursive ls --depth=-1 | cut -d ' ' -f 1 | tr '\n' ' '`;
-$PNPM_RECURSIVE exec -- pnpm publish $DIST_TAG
+$PNPM_RECURSIVE publish $DIST_TAG
