fba30909bf8d67511ac70b38af9f3b8a0748d249
LAN Xingcan <thynson@live.com>
2020-04-07 18:06:47 +0800

    chore: change version strategy
    Now each package will not be kept synchronized any more.

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,10 +1,2 @@
-# First dry run to change all package version
-DIST_TAG=$2
-if [[ -n "$DIST_TAG" ]]; then
-  DIST_TAG="--tag=$DIST_TAG"
-fi
-pnpm recursive run cleanup
-pnpm recursive run prepare
-PNPM_RECURSIVE='pnpm recursive --workspace-concurrency=1 --filter ./packages'
-$PNPM_RECURSIVE exec -- pnpm version --no-git-tag-version $1
-$PNPM_RECURSIVE publish $DIST_TAG
+pnpm recursive run clean
+pnpm recursive run prepare && pnpm recursive publish $@
