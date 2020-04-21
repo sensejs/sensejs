e84f4981deab02d8fc08c3a5ff5527d3e5eed6e2
LAN Xingcan <thynson@live.com>
2020-04-21 12:31:55 +0800

    feat(logger): provide default label name based on constructor

--- packages/core/src/logger.ts
+++ packages/core/src/logger.ts
@@ -1,9 +1,8 @@
 import {Class} from './interfaces';
 import {Inject, Optional} from './decorators';
-import {consoleLogger, DecoratorBuilder, Logger} from '@sensejs/utility';
+import {consoleLogger, DecoratorBuilder, Logger, Transformer} from '@sensejs/utility';
 
 export {consoleLogger, Logger} from '@sensejs/utility';
-
 
 export abstract class LoggerBuilder {
   abstract build(loggerLabel: string): Logger;
@@ -14,19 +13,26 @@ export abstract class LoggerBuilder {
  */
 export const LOGGER_BUILDER_SYMBOL = LoggerBuilder;
 
-export function InjectLogger(name?: string | Function) {
-  const labelName = typeof name === 'function' ? name.name : typeof name === 'string' ? name : '';
-  const option = {
-    transform: (builder?: LoggerBuilder) => builder?.build(labelName) ?? consoleLogger,
+function loggerTransformer(label: string): Transformer<LoggerBuilder | undefined, Logger> {
+  return (builder?: LoggerBuilder) => {
+    return builder?.build(label) ?? consoleLogger;
   };
+}
+
+export function InjectLogger(name?: string | Function) {
+  const labelName = typeof name === 'function' ? name.name : typeof name === 'string' ? name : undefined;
   return new DecoratorBuilder('InjectLogger')
     .whenApplyToConstructorParam(<T extends Class>(target: T, index: number) => {
       Optional()(target, undefined, index);
-      Inject(LoggerBuilder, option)(target, undefined, index);
+      return Inject(LoggerBuilder, {
+        transform: loggerTransformer(labelName ?? target.name ?? ''),
+      })(target, undefined, index);
     })
     .whenApplyToInstanceMethodParam(<T extends {}>(target: T, name: (string | symbol), index: number) => {
       Optional()(target, name, index);
-      Inject(LoggerBuilder, option)(target, name, index);
+      Inject(LoggerBuilder, {
+        transform: loggerTransformer(labelName ?? target.constructor.name ?? ''),
+      })(target, name, index);
     })
     .build();
 }
