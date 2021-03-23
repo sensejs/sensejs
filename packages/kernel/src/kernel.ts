7031a46526fae75138969d6574f4e23e0fe0898e
LAN Xingcan <thynson@live.com>
2021-03-24 05:15:26 +0800

    feat: initial ioc kernel

---
+++ packages/kernel/src/kernel.ts
@@ -0,0 +1,244 @@
+import {Class, Constructor} from '@sensejs/utility';
+
+export type ServiceId<T> = Class<T> | string | symbol;
+
+export enum BindingType {
+  CONSTANT = 'CONSTANT',
+  INSTANCE = 'INSTANCE',
+  FACTORY = 'FACTORY',
+  ALIAS = 'ALIAS',
+}
+
+export enum Scope {
+  SINGLETON = 'SINGLETON',
+  REQUEST = 'REQUEST',
+  TRANSIENT = 'TRANSIENT',
+}
+
+export function untransformed<T>(input: T) {
+  return input;
+}
+
+export interface ParamInjectionMetadata<T> {
+  index: number;
+  id: ServiceId<T>;
+  optional: boolean;
+  transform: (input: T) => any;
+}
+
+export interface ConstantBinding<T> {
+  type: BindingType.CONSTANT;
+  id: ServiceId<T>;
+  value: T;
+}
+
+export interface InstanceBinding<T> {
+  type: BindingType.INSTANCE;
+  id: ServiceId<T>;
+  constructor: Constructor<T>;
+  paramInjectionMetadata: ParamInjectionMetadata<any>[];
+  scope: Scope;
+}
+
+export interface FactoryBinding<T, F = any> {
+  type: BindingType.FACTORY;
+  id: ServiceId<T>;
+  factoryId: ServiceId<(...args: any[]) => T>;
+  factoryScope: Scope;
+  factoryParamInjectionMetadata: ParamInjectionMetadata<any>[];
+}
+
+export interface AliasBinding<T> {
+  type: BindingType.ALIAS;
+  id: ServiceId<T>;
+  canonicalId: ServiceId<any>;
+}
+
+export type Binding<T> = ConstantBinding<T> | InstanceBinding<T> | FactoryBinding<T> | AliasBinding<T>;
+
+export enum InstructionCode {
+  PLAN = 'PLAN',
+  CONSTRUCT = 'CONSTRUCT',
+  TRANSFORM = 'TRANSFORM',
+}
+
+interface PlanInstruction {
+  code: InstructionCode.PLAN;
+  target: ServiceId<any>;
+  optional: boolean;
+}
+
+interface ConstructInstruction {
+  code: InstructionCode.CONSTRUCT;
+  serviceId: ServiceId<any>;
+  constructor: Constructor<any>;
+  paramCount: number;
+  cacheScope: Scope;
+}
+
+interface TransformInstruction {
+  code: InstructionCode.TRANSFORM;
+  transformer: (input: any) => any;
+}
+
+export type Instruction = PlanInstruction | ConstructInstruction | TransformInstruction;
+
+class ResolveContext {
+  readonly planingSet: Set<ServiceId<any>> = new Set();
+  readonly instructions: Instruction[] = [];
+  readonly requestSingletonCache: Map<any, any> = new Map();
+  readonly stack: any[] = [];
+
+  constructor(
+    readonly bindingMap: Map<ServiceId<any>, Binding<any>>,
+    readonly globalSingletonCache: Map<any, any>,
+    readonly target: ServiceId<any>,
+  ) {
+    this.performPlan({code: InstructionCode.PLAN, optional: false, target});
+  }
+
+  resolve() {
+    for (;;) {
+      const instruction = this.instructions.pop();
+      if (!instruction) {
+        return this.stack[0];
+      }
+
+      switch (instruction.code) {
+        case InstructionCode.CONSTRUCT:
+          this.performConstruction(instruction);
+          break;
+
+        case InstructionCode.PLAN:
+          this.performPlan(instruction);
+          break;
+        case InstructionCode.TRANSFORM:
+          this.performTransform(instruction);
+          break;
+        default: {
+          const never: never = instruction;
+        }
+      }
+    }
+  }
+
+  private internalGetBinding(target: ServiceId<any>) {
+    let binding;
+    for (;;) {
+      binding = this.bindingMap.get(target);
+      if (!binding) {
+        return;
+      }
+      if (binding.type !== BindingType.ALIAS) {
+        return binding;
+      }
+      target = binding.canonicalId;
+    }
+  }
+
+  private performPlan(instruction: PlanInstruction) {
+    const {target, optional} = instruction;
+    if (this.planingSet.has(target)) {
+      throw new Error('circular detected');
+    }
+    const binding = this.internalGetBinding(target);
+    if (!binding) {
+      if (optional) {
+        this.stack.push(undefined);
+        return;
+      } else {
+        throw new Error('No binding found');
+      }
+    }
+    switch (binding.type) {
+      case BindingType.CONSTANT:
+        this.stack.push(binding.value);
+        break;
+
+      case BindingType.INSTANCE:
+        {
+          const {constructor, paramInjectionMetadata, scope, id} = binding;
+          if (scope === Scope.SINGLETON) {
+            if (this.globalSingletonCache.has(id)) {
+              this.stack.push(this.globalSingletonCache.get(id));
+              return;
+            }
+          } else if (scope === Scope.REQUEST) {
+            if (this.requestSingletonCache.has(id)) {
+              this.stack.push(this.requestSingletonCache.get(id));
+              return;
+            }
+          }
+          this.planingSet.add(target);
+          this.instructions.push({
+            code: InstructionCode.CONSTRUCT,
+            constructor,
+            serviceId: target,
+            paramCount: paramInjectionMetadata.length,
+            cacheScope: scope,
+          });
+          paramInjectionMetadata.reduceRight((unused, pim) => {
+            const {id, optional} = pim;
+            this.instructions.push({
+              code: InstructionCode.PLAN,
+              target: id,
+              optional,
+            });
+            return null;
+          }, null);
+        }
+        break;
+      case BindingType.FACTORY: {
+        throw new Error('Unsupported');
+      }
+    }
+  }
+
+  private performConstruction(instruction: ConstructInstruction) {
+    const {paramCount, constructor, cacheScope, serviceId} = instruction;
+    if (cacheScope === Scope.SINGLETON) {
+      if (this.globalSingletonCache.has(serviceId)) {
+        throw new Error('Reconstruct a global singleton');
+      }
+    }
+
+    if (cacheScope === Scope.REQUEST) {
+      if (this.requestSingletonCache.has(serviceId)) {
+        throw new Error('Reconstruct a request-scope singleton');
+      }
+    }
+    const args = this.stack.splice(this.stack.length - paramCount);
+    const result = Reflect.construct(constructor, args);
+    if (cacheScope === Scope.REQUEST) {
+      this.requestSingletonCache.set(serviceId, result);
+    } else if (cacheScope === Scope.SINGLETON) {
+      this.globalSingletonCache.set(serviceId, result);
+    }
+    this.stack.push(result);
+    this.planingSet.delete(serviceId);
+  }
+
+  private performTransform(instruction: TransformInstruction) {
+    const {transformer} = instruction;
+    const top = this.stack.pop();
+    this.stack.push(transformer(top));
+  }
+}
+
+export class Kernel {
+  private bindingMap: Map<ServiceId<any>, Binding<any>> = new Map();
+  private singletonCache: Map<ServiceId<any>, any> = new Map();
+
+  addBinding<T>(binding: Binding<T>) {
+    const id = binding.id;
+    if (this.bindingMap.has(id)) {
+      throw new Error('Duplicated');
+    }
+    this.bindingMap.set(id, binding);
+  }
+
+  resolve<T>(serviceId: ServiceId<T>): T {
+    const requestContext = new ResolveContext(this.bindingMap, this.singletonCache, serviceId);
+    return requestContext.resolve();
+  }
+}
---
+++ packages/kernel/tests/kernel.test.ts
@@ -0,0 +1,196 @@
+import {BindingType, Kernel, Scope} from '../src/kernel';
+import {untransformed} from '../lib/kernel';
+
+describe('Kernel', () => {
+  test('simple resolve', () => {
+    const kernel = new Kernel();
+    const value = 1;
+
+    class Foo {
+      constructor(readonly param: number) {}
+    }
+
+    kernel.addBinding({
+      type: BindingType.CONSTANT,
+      value,
+      id: '1',
+    });
+
+    kernel.addBinding({
+      id: Foo,
+      type: BindingType.INSTANCE,
+      constructor: Foo,
+      paramInjectionMetadata: [{id: '1', index: 0, optional: false, transform: untransformed}],
+      scope: Scope.SINGLETON,
+    });
+
+    const result = kernel.resolve(Foo);
+    expect(result).toBeInstanceOf(Foo);
+    expect(result.param).toBe(value);
+  });
+
+  test('optional', () => {
+    const kernel = new Kernel();
+    const value = 1;
+
+    class Foo {
+      constructor(readonly param: number, readonly optional?: any) {}
+    }
+
+    kernel.addBinding({
+      type: BindingType.CONSTANT,
+      value,
+      id: '1',
+    });
+
+    kernel.addBinding({
+      id: Foo,
+      type: BindingType.INSTANCE,
+      constructor: Foo,
+      paramInjectionMetadata: [
+        {id: '1', index: 0, optional: false, transform: untransformed},
+        {id: 'optional', index: 1, optional: true, transform: untransformed},
+      ],
+      scope: Scope.SINGLETON,
+    });
+
+    const result = kernel.resolve(Foo);
+    expect(result).toBeInstanceOf(Foo);
+    expect(result.param).toBe(value);
+    expect(result.optional).toBe(undefined);
+  });
+
+  test('singleton alias', () => {
+    const kernel = new Kernel();
+
+    class Foo {
+      constructor() {}
+    }
+
+    class Bar {
+      constructor(readonly param1: any, readonly param2: any) {}
+    }
+
+    kernel.addBinding({
+      id: Foo,
+      type: BindingType.INSTANCE,
+      constructor: Foo,
+      paramInjectionMetadata: [],
+      scope: Scope.SINGLETON,
+    });
+
+    kernel.addBinding({
+      id: 'alias',
+      type: BindingType.ALIAS,
+      canonicalId: Foo,
+    });
+
+    kernel.addBinding({
+      id: Bar,
+      type: BindingType.INSTANCE,
+      constructor: Bar,
+      paramInjectionMetadata: [
+        {id: Foo, index: 0, optional: false, transform: untransformed},
+        {id: 'alias', index: 1, optional: false, transform: untransformed},
+      ],
+      scope: Scope.SINGLETON,
+    });
+
+    const result = kernel.resolve(Bar);
+    expect(result.param1).toBeInstanceOf(Foo);
+    expect(result.param2).toBeInstanceOf(Foo);
+    expect(result.param2).toBe(result.param1);
+  });
+
+  test('circular', () => {
+    class Foo {}
+
+    class Bar {}
+
+    const kernel = new Kernel();
+
+    kernel.addBinding({
+      id: Foo,
+      type: BindingType.INSTANCE,
+      constructor: Foo,
+      paramInjectionMetadata: [{id: Bar, index: 0, optional: false, transform: untransformed}],
+      scope: Scope.SINGLETON,
+    });
+    kernel.addBinding({
+      id: Bar,
+      type: BindingType.INSTANCE,
+      constructor: Bar,
+      paramInjectionMetadata: [{id: Foo, index: 0, optional: false, transform: untransformed}],
+      scope: Scope.SINGLETON,
+    });
+
+    expect(() => kernel.resolve(Foo)).toThrow();
+  });
+
+  test('scope', () => {
+    const kernel = new Kernel();
+
+    class GlobalSingleton {
+      constructor() {}
+    }
+
+    kernel.addBinding({
+      id: GlobalSingleton,
+      type: BindingType.INSTANCE,
+      constructor: GlobalSingleton,
+      paramInjectionMetadata: [],
+      scope: Scope.SINGLETON,
+    });
+
+    class RequestSingleton {
+      constructor(readonly param1: GlobalSingleton) {}
+    }
+
+    kernel.addBinding({
+      id: RequestSingleton,
+      type: BindingType.INSTANCE,
+      constructor: RequestSingleton,
+      paramInjectionMetadata: [{id: GlobalSingleton, index: 0, optional: false, transform: untransformed}],
+      scope: Scope.REQUEST,
+    });
+
+    class Transient {
+      constructor(readonly param1: GlobalSingleton, readonly param2: RequestSingleton) {}
+    }
+
+    kernel.addBinding({
+      id: Transient,
+      type: BindingType.INSTANCE,
+      constructor: Transient,
+      paramInjectionMetadata: [
+        {id: GlobalSingleton, index: 0, optional: false, transform: untransformed},
+        {id: RequestSingleton, index: 1, optional: false, transform: untransformed},
+      ],
+      scope: Scope.TRANSIENT,
+    });
+
+    class Root {
+      constructor(readonly param1: Transient, readonly param2: Transient) {}
+    }
+
+    kernel.addBinding({
+      id: Root,
+      type: BindingType.INSTANCE,
+      constructor: Root,
+      paramInjectionMetadata: [
+        {id: Transient, index: 0, optional: false, transform: untransformed},
+        {id: Transient, index: 1, optional: false, transform: untransformed},
+      ],
+      scope: Scope.TRANSIENT,
+    });
+
+    const root = kernel.resolve(Root);
+    expect(root.param1).toBeInstanceOf(Transient);
+    expect(root.param2).toBeInstanceOf(Transient);
+    expect(root.param1).not.toBe(root.param2);
+    expect(root.param1.param1).toBeInstanceOf(GlobalSingleton);
+    expect(root.param2.param1).toBeInstanceOf(GlobalSingleton);
+    expect(root.param1.param1).toBe(root.param2.param1);
+    expect(root.param1.param2).toBe(root.param2.param2);
+  });
+});
