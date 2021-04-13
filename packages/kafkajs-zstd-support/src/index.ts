a5a1a1a9affb80c6456dc1b78ede0ecae119bdd7
LAN Xingcan <thynson@live.com>
2021-04-13 18:46:48 +0800

    fix: remove check whether zstd codec is present

--- packages/kafkajs-zstd-support/src/index.ts
+++ packages/kafkajs-zstd-support/src/index.ts
@@ -1,10 +1,6 @@
 import {CompressionTypes, CompressionCodecs} from 'kafkajs';
 import zstd from 'zstd-napi';
 
-if (CompressionCodecs[CompressionTypes.ZSTD]) {
-  throw new Error('Zstd Codec has already been defined for kafkajs');
-}
-
 const zstdKafkaCodec = () => {
   const compressor = new zstd.Compressor();
   const decompressor = new zstd.Decompressor();
