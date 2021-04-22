64d2df7e1413d4466f1822ca4f58ad7661ef52db
LAN Xingcan <thynson@live.com>
2021-04-22 08:48:43 +0800

    fix(kafkajs-standalone): simple and transactional kafkajs message producer should be exported

--- packages/kafkajs-standalone/src/index.ts
+++ packages/kafkajs-standalone/src/index.ts
@@ -4,3 +4,5 @@ export * from './worker-synchronizer';
 export * from './message-producer';
 export * from './simple-provider';
 export * from './pooled-provider';
+export * from './simple-message-producer';
+export * from './transactional-message-producer';
