ab9172b4cfd467a65cda8c8ac2104c59178e2cac
LAN Xingcan <thynson@live.com>
2021-04-27 16:44:58 +0800

    test: fix test

--- packages/kafkajs-zstd-support/tests/kafkajs.e2e-spec.ts
+++ packages/kafkajs-zstd-support/tests/kafkajs.e2e-spec.ts
@@ -1,7 +1,6 @@
 import '../src';
 import {CompressionTypes, Kafka} from 'kafkajs';
 import config from 'config';
-console.log('node env', process.env.NODE_ENV);
 
 test('ZSTD E2E test', async () => {
   const topic = 'test-zstd.' + Date.now();
@@ -9,19 +8,19 @@ test('ZSTD E2E test', async () => {
   const kafka = new Kafka({brokers: config.get('kafka.connectOption.brokers')});
   const producer = kafka.producer();
   await producer.connect();
-  await producer.send({topic, messages: [{value: topic}], compression: CompressionTypes.ZSTD});
+  await producer.send({topic, messages: [{value: data}], compression: CompressionTypes.ZSTD});
   await producer.disconnect();
   const consumer = kafka.consumer({groupId: 'zstd-test-group.' + Date.now()});
-  await consumer.subscribe({topic});
+  await consumer.subscribe({topic, fromBeginning: true});
   await new Promise((resolve, reject) => {
     consumer
       .run({
         eachMessage: async (param) => {
-          expect(param.message.value).toBe(data);
+          expect(param.message.value!.toString()).toBe(data);
           consumer.stop().then(resolve);
         },
       })
       .catch(reject);
   });
   await consumer.disconnect();
-});
+}, 30000);
