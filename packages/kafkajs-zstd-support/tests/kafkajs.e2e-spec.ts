c1fd320eb97a7d57e5945639872671b3e722b4b8
LAN Xingcan <thynson@live.com>
2021-04-27 09:55:54 +0800

    test: add e2e test for zstd-support

---
+++ packages/kafkajs-zstd-support/tests/kafkajs.e2e-spec.ts
@@ -0,0 +1,26 @@
+import '../src';
+import {CompressionTypes, Kafka} from 'kafkajs';
+import config from 'config';
+
+test('ZSTD E2E test', async () => {
+  const topic = 'test-zstd.' + Date.now();
+  const data = 'test-data' + Date.now();
+  const kafka = new Kafka({brokers: config.get('kafka.connectOption.brokers')});
+  const producer = kafka.producer();
+  await producer.connect();
+  await producer.send({topic, messages: [{value: topic}], compression: CompressionTypes.ZSTD});
+  await producer.disconnect();
+  const consumer = kafka.consumer();
+  await consumer.subscribe({topic});
+  await new Promise((resolve, reject) => {
+    consumer
+      .run({
+        eachMessage: async (param) => {
+          expect(param.message.value).toBe(data);
+          consumer.stop().then(resolve);
+        },
+      })
+      .catch(reject);
+  });
+  await consumer.disconnect();
+});
