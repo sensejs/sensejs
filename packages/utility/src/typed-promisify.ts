313a8ecc2d26eacd55988629033ef77232004a27
LAN Xingcan <thynson@live.com>
2021-04-28 18:51:00 +0800

    feat: improve typed promsify

--- packages/utility/src/typed-promisify.ts
+++ packages/utility/src/typed-promisify.ts
@@ -1,40 +1,129 @@
 export type NodeCallback<R, E = Error> = (...args: [Error] | [null | undefined, R]) => any;
-export type AggregatedNodeCallback<R extends unknown[], E = Error> = (
-  ...args: [Error] | [null | undefined, ...R]
-) => any;
+export type AggregatedNodeCallback<R extends any[], E = Error> = (...args: [Error] | [null | undefined, ...R]) => any;
 
-export function promisify<R, E, T extends unknown[]>(f: (...param: [...T, NodeCallback<R, E>]) => any) {
-  return (...args: [...T]): Promise<R> =>
-    new Promise<R>((resolve, reject) => {
-      return f(...args, (...args: [Error, ...any] | [null | undefined, R]) => {
+type InferParam<T> = T extends (...args: [Error] | [null | undefined, infer R]) => any
+  ? R
+  : T extends (error: Error | null, result: infer R) => any // Workaround common but inaccurate callback typing pattern
+  ? R
+  : never;
+
+type InferAggregateParam<T> = T extends (...args: [Error] | [null | undefined, ...infer R]) => any
+  ? R
+  : T extends (error: Error | null, ...args: infer R) => any // Workaround common but inaccurate callback typing pattern
+  ? R
+  : never;
+
+type PromisifyOne<T> = T extends [...infer A, infer U] ? (...args: A) => Promise<InferParam<U>> : never;
+
+type PromisifyAggregated<T> = T extends [...infer A, infer U] ? (...args: A) => Promise<InferAggregateParam<U>> : never;
+
+type OverloadToUnion<T> = T extends {
+  (...o: infer U1): void;
+  (...o: infer U2): void;
+  (...o: infer U3): void;
+  (...o: infer U4): void;
+}
+  ? U1 | U2 | U3 | U4
+  : T extends {
+      (...o: infer U1): void;
+      (...o: infer U2): void;
+      (...o: infer U3): void;
+    }
+  ? U1 | U2 | U3
+  : T extends {
+      (...o: infer U1): void;
+      (...o: infer U2): void;
+    }
+  ? U1 | U2
+  : T extends {
+      (...o: infer U1): void;
+    }
+  ? U1
+  : never;
+
+type InferOverload<T, A> = T extends [...infer P, infer U] ? (A extends P ? InferParam<U> : never) : never;
+
+type ResolveOverloadedCall<T, A> = T extends {
+  (...o: infer U1): void;
+  (...o: infer U2): void;
+  (...o: infer U3): void;
+  (...o: infer U4): void;
+}
+  ? InferOverload<U1, A> | InferOverload<U2, A> | InferOverload<U3, A> | InferOverload<U4, A>
+  : T extends {
+      (...o: infer U1): void;
+      (...o: infer U2): void;
+      (...o: infer U3): void;
+    }
+  ? InferOverload<U1, A> | InferOverload<U2, A> | InferOverload<U3, A>
+  : T extends {
+      (...o: infer U1): void;
+      (...o: infer U2): void;
+    }
+  ? InferOverload<U1, A> | InferOverload<U2, A>
+  : T extends {
+      (...o: infer U1): void;
+    }
+  ? InferOverload<U1, A>
+  : never;
+
+export type InferSpreadOverload<T, A> = T extends [...infer P, infer U]
+  ? A extends P
+    ? InferAggregateParam<U>
+    : never
+  : never;
+
+export type ResolveOverloadedCallAndSpread<T, A> = T extends {
+  (...o: infer U1): void;
+  (...o: infer U2): void;
+  (...o: infer U3): void;
+  (...o: infer U4): void;
+}
+  ? InferSpreadOverload<U1, A> | InferSpreadOverload<U2, A> | InferSpreadOverload<U3, A> | InferSpreadOverload<U4, A>
+  : T extends {
+      (...o: infer U1): void;
+      (...o: infer U2): void;
+      (...o: infer U3): void;
+    }
+  ? InferSpreadOverload<U1, A> | InferSpreadOverload<U2, A> | InferSpreadOverload<U3, A>
+  : T extends {
+      (...o: infer U1): void;
+      (...o: infer U2): void;
+    }
+  ? InferSpreadOverload<U1, A> | InferSpreadOverload<U2, A>
+  : T extends {
+      (...o: infer U1): void;
+    }
+  ? InferSpreadOverload<U1, A>
+  : never;
+
+/**
+ * Convert union function types to intersection
+ * e.g. Convert `((a: number) => void) | (a: number, b: string) => void)` to
+ * `((a: number) => void) & (a: number, b: string) => void)`.
+ */
+type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (k: infer I) => void ? I : never;
+
+export type TypedPromisify<T> = UnionToIntersection<PromisifyOne<OverloadToUnion<T>>>;
+
+export type TypedAggregatedPromisify<T> = UnionToIntersection<PromisifyAggregated<OverloadToUnion<T>>>;
+
+export function promisify<F extends Function>(fn: F): TypedPromisify<F> {
+  return ((...args: any[]): Promise<any> =>
+    new Promise<any>((resolve, reject) => {
+      return fn(...args, (...args: [Error] | [null | undefined, any]) => {
         if (args[0]) {
           return reject(args[0]);
         }
         return resolve(args[1]);
       });
-    });
+    })) as TypedPromisify<F>;
 }
 
-export function promisifiedCall<R, E, T extends unknown[]>(
-  f: (...param: [...T, NodeCallback<R, E>]) => any,
-  ...param: T
-): Promise<R> {
-  return new Promise<R>((resolve, reject) =>
-    f(...param, (...args: [Error, ...any] | [null | undefined, R]) => {
-      if (args[0]) {
-        return reject(args[0]);
-      }
-      return resolve(args[1]);
-    }),
-  );
-}
-
-export function aggregatedPromisify<R extends unknown[], E, T extends unknown[]>(
-  f: (...param: [...T, AggregatedNodeCallback<R, E>]) => any,
-) {
-  return (...args: [...T]): Promise<R> =>
-    new Promise<R>((resolve, reject) => {
-      return f(...args, (...args: [Error, ...any] | [null | undefined, ...R]) => {
+export function aggregatedPromisify<F extends Function>(fn: F): TypedAggregatedPromisify<F> {
+  return ((...args: any[]): Promise<any> => {
+    return new Promise<any>((resolve, reject) => {
+      return fn(...args, (...args: any[]) => {
         if (args[0]) {
           return reject(args[0]);
         }
@@ -42,19 +131,34 @@ export function aggregatedPromisify<R extends unknown[], E, T extends unknown[]>
         return resolve(r);
       });
     });
+  }) as TypedAggregatedPromisify<F>;
 }
 
-export function aggregatedPromisifiedCall<R extends unknown[], E, T extends unknown[]>(
-  f: (...param: [...T, AggregatedNodeCallback<R, E>]) => any,
-  ...param: T
-): Promise<R> {
-  return new Promise<R>((resolve, reject) =>
-    f(...param, (...args: [Error, ...any] | [null | undefined, ...R]) => {
+export function promisifiedCall<F extends Function, A extends any[]>(
+  fn: F,
+  ...rest: A
+): Promise<ResolveOverloadedCall<F, A>> {
+  return new Promise<any>((resolve, reject) => {
+    return fn(...rest, (...args: [Error] | [null | undefined, any]) => {
+      if (args[0]) {
+        return reject(args[0]);
+      }
+      return resolve(args[1]);
+    });
+  });
+}
+
+export function aggregatedPromisifiedCall<F extends Function, A extends any[]>(
+  fn: F,
+  ...rest: A
+): Promise<ResolveOverloadedCallAndSpread<F, A>> {
+  return new Promise<any>((resolve, reject) => {
+    return fn(...rest, (...args: any[]) => {
       if (args[0]) {
         return reject(args[0]);
       }
       const [, ...r] = args;
       return resolve(r);
-    }),
-  );
+    });
+  });
 }
--- packages/utility/tests/typed-promisify.test.ts
+++ packages/utility/tests/typed-promisify.test.ts
@@ -19,6 +19,69 @@ function g3(a: number, b: string, cb: (...e: [Error] | [null, ...number[]]) => v
   return cb(null);
 }
 
+function overload(cb: (e: Error | null, value: number) => void): void;
+function overload(param1: boolean, cb: (e: Error | null, value: boolean) => void): void;
+
+function overload<T extends any[]>(...args: T) {
+  const cb = args.pop() as any;
+  switch (args.length) {
+    case 0:
+      cb(null, 1);
+      return;
+    case 1:
+      cb(null, true);
+      return;
+    default:
+      throw new Error();
+  }
+}
+function overload2(cb: (e: Error | null, value: number, value2: boolean) => void): void;
+function overload2(param1: boolean, cb: (e: Error | null, value: boolean, value2: number) => void): void;
+
+function overload2<T extends any[]>(...args: T) {
+  const cb = args.pop() as any;
+  switch (args.length) {
+    case 0:
+      cb(null, 1);
+      return;
+    case 1:
+      cb(null, true);
+      return;
+    default:
+      throw new Error();
+  }
+}
+
+test('resolve overload', async () => {
+  const fn = promisify(overload);
+  let r1: number = await promisifiedCall(overload);
+  expect(typeof r1 === 'number');
+  r1 = await fn();
+  expect(typeof r1 === 'number');
+
+  let r2: boolean = await promisifiedCall(overload, true);
+  expect(typeof r2 === 'boolean');
+  r2 = await fn(true);
+  expect(typeof r2 === 'boolean');
+
+  const pf = await aggregatedPromisify(overload2);
+
+  let result1 = await aggregatedPromisifiedCall(overload2);
+  expect(typeof result1[0]! === 'number');
+  expect(typeof result1[1]! === 'boolean');
+
+  result1 = await pf();
+  expect(typeof result1[0]! === 'number');
+  expect(typeof result1[1]! === 'boolean');
+
+  let result2 = await aggregatedPromisifiedCall(overload2, true);
+  expect(typeof result2[0]! === 'boolean');
+  expect(typeof result2[1]! === 'number');
+  result2 = await pf(false);
+  expect(typeof result2[0]! === 'boolean');
+  expect(typeof result2[1]! === 'number');
+});
+
 test('promisify', async () => {
   await expect(promisify(f1)()).resolves.toBe(42);
   await expect(promisify(f2)()).rejects.toBeInstanceOf(Error);
