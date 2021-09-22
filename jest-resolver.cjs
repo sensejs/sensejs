73fce84318c9c6a9a4755bea2ac44373f66d3458
LAN Xingcan <thynson@live.com>
2021-09-22 16:50:20 +0800

    fix: for node 12

--- jest-resolver.cjs
+++ jest-resolver.cjs
@@ -10,13 +10,13 @@ const resolver = require('enhanced-resolve').create.sync({
 
 module.exports = function (request, options) {
   const resolver = enhancedResolve.create.sync({
-    conditionNames: ['node'].concat(options.conditions ?? []),
+    conditionNames: ['node'].concat(options.conditions || []),
     fullySpecified: false,
     extensions: [".js", ".json", ".node", ".ts", ".tsx"]
   })
   if (options.basedir.startsWith(__dirname)
     && options.basedir.indexOf('/node_modules/') < 0) {
-    if ((options.conditions ?? []).indexOf('import') >= 0 && request.startsWith('.') && !/.[cm]?js$/.test(request)) {
+    if ((options.conditions || []).indexOf('import') >= 0 && request.startsWith('.') && !/.[cm]?js$/.test(request)) {
       throw new Error('File extensions cannot be omitted when importing a file from ESM module')
     }
     request = request.replace(/\.js$/, '');
