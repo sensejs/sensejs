343d64d1f6b5a750682501bf35a82263fa054ad3
LAN Xingcan <thynson@live.com>
2022-06-07 16:24:55 +0800

    chore: fix tags.sh due to breaking change of pnpm 7

--- scripts/tags.sh
+++ scripts/tags.sh
@@ -1,1 +1,1 @@
-pnpm recursive --filter ./packages exec -- sh -c 'git tag `jq -r .name package.json`@`jq -r .version package.json` || true'
+pnpm recursive --filter './packages/*' exec -- sh -c 'git tag `jq -r .name package.json`@`jq -r .version package.json` || true'
