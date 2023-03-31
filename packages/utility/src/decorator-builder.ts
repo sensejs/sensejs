1736c579b67dd775d6584e641024d82a6eec163f
LAN Xingcan <thynson@live.com>
2023-03-31 14:36:40 +0800

    refactor(utility)!: turn all typescript private field into ECMA private field

--- packages/utility/src/decorator-builder.ts
+++ packages/utility/src/decorator-builder.ts
@@ -61,54 +61,54 @@ type NarrowTo<T> = Decorator extends T ? T : unknown;
 export class DecoratorBuilder {
   constructor(private decoratorName: string, strictChecking = true) {
     if (!strictChecking) {
-      this.applyToConstructorParam = noop;
-      this.applyToStaticMethodParam = noop;
-      this.applyToInstanceMethodParam = noop;
-      this.applyToInstanceProperty = noop;
-      this.applyToStaticProperty = noop;
-      this.applyToInstanceMethod = noop;
-      this.applyToStaticMethod = noop;
-      this.applyToClass = noop;
+      this.#applyToConstructorParam = noop;
+      this.#applyToStaticMethodParam = noop;
+      this.#applyToInstanceMethodParam = noop;
+      this.#applyToInstanceProperty = noop;
+      this.#applyToStaticProperty = noop;
+      this.#applyToInstanceMethod = noop;
+      this.#applyToStaticMethod = noop;
+      this.#applyToClass = noop;
     }
   }
 
   whenApplyToConstructor(fn: ClassDecorator): this {
-    this.applyToClass = fn;
+    this.#applyToClass = fn;
     return this;
   }
 
   whenApplyToInstanceProperty(fn: InstancePropertyDecorator): this {
-    this.applyToInstanceProperty = fn;
+    this.#applyToInstanceProperty = fn;
     return this;
   }
 
   whenApplyToStaticProperty(fn: StaticPropertyDecorator): this {
-    this.applyToStaticProperty = fn;
+    this.#applyToStaticProperty = fn;
     return this;
   }
 
   whenApplyToInstanceMethod(fn: InstanceMethodDecorator): this {
-    this.applyToInstanceMethod = fn;
+    this.#applyToInstanceMethod = fn;
     return this;
   }
 
   whenApplyToStaticMethod(fn: StaticMethodDecorator): this {
-    this.applyToStaticMethod = fn;
+    this.#applyToStaticMethod = fn;
     return this;
   }
 
   whenApplyToConstructorParam(fn: <T extends Class>(ctr: T, index: number) => T | void): this {
-    this.applyToConstructorParam = <T extends Class>(ctr: T, name: any, index: number) => fn(ctr, index);
+    this.#applyToConstructorParam = <T extends Class>(ctr: T, name: any, index: number) => fn(ctr, index);
     return this;
   }
 
   whenApplyToStaticMethodParam(fn: StaticMethodParamDecorator): this {
-    this.applyToStaticMethodParam = fn;
+    this.#applyToStaticMethodParam = fn;
     return this;
   }
 
   whenApplyToInstanceMethodParam(fn: InstanceMethodParamDecorator): this {
-    this.applyToInstanceMethodParam = fn;
+    this.#applyToInstanceMethodParam = fn;
     return this;
   }
 
@@ -126,57 +126,56 @@ export class DecoratorBuilder {
       } else if (typeof name !== 'undefined') {
         return this.applyInstanceDecorator(target, name, descriptor);
       }
-      return this.applyToWrongPlace();
+      return this.#applyToWrongPlace();
     };
     return result as NarrowTo<T>;
   }
 
-  private applyToClass: ClassDecorator = () => {
+  #applyToClass: ClassDecorator = () => {
     throw new Error(`@${this.decoratorName} cannot apply to class`);
   };
 
-  private applyToInstanceProperty: InstancePropertyDecorator = () => {
+  #applyToInstanceProperty: InstancePropertyDecorator = () => {
     throw new Error(`@${this.decoratorName} cannot apply to property`);
   };
 
-  private applyToStaticProperty: StaticPropertyDecorator = () => {
+  #applyToStaticProperty: StaticPropertyDecorator = () => {
     throw new Error(`@${this.decoratorName} cannot apply to static property`);
   };
 
-  private applyToInstanceMethod: InstanceMethodDecorator = () => {
+  #applyToInstanceMethod: InstanceMethodDecorator = () => {
     throw new Error(`@${this.decoratorName} cannot apply to method`);
   };
 
-  private applyToStaticMethod: StaticMethodDecorator = () => {
+  #applyToStaticMethod: StaticMethodDecorator = () => {
     throw new Error(`@${this.decoratorName} cannot apply to static method`);
   };
 
-  private applyToConstructorParam: <T extends Class>(ctr: T, name: any, index: number) => void | T = () => {
+  #applyToConstructorParam: <T extends Class>(ctr: T, name: any, index: number) => void | T = () => {
     throw new Error(`@${this.decoratorName} cannot apply to param of class constructor`);
   };
 
-  private applyToStaticMethodParam: StaticMethodParamDecorator = () => {
+  #applyToStaticMethodParam: StaticMethodParamDecorator = () => {
     throw new Error(`@${this.decoratorName} cannot apply to param of class static method`);
   };
 
-  private applyToInstanceMethodParam: InstanceMethodParamDecorator = () => {
+  #applyToInstanceMethodParam: InstanceMethodParamDecorator = () => {
     throw new Error(`@${this.decoratorName} cannot apply to param of class instance method`);
   };
 
-  private applyToWrongPlace: () => void = () => {
+  #applyToWrongPlace: () => void = () => {
     throw new Error(`@${this.decoratorName} cannot be applied here`);
   };
 
   private applyConstructorDecorator<C extends Class>(target: object | C, descriptor: unknown) {
-    const {applyToClass, applyToConstructorParam} = this;
     if (typeof target === 'function') {
       if (typeof descriptor === 'number') {
-        return applyToConstructorParam(target, undefined, descriptor);
+        return this.#applyToConstructorParam(target, undefined, descriptor);
       } else if (typeof descriptor === 'undefined') {
-        return applyToClass(target);
+        return this.#applyToClass(target);
       }
     }
-    return this.applyToWrongPlace();
+    return this.#applyToWrongPlace();
   }
 
   private applyInstanceDecorator<F extends Function, P extends {}, K extends keyof P>(
@@ -184,14 +183,12 @@ export class DecoratorBuilder {
     name: K,
     descriptor?: number | TypedPropertyDescriptor<F>,
   ) {
-    const {applyToInstanceMethodParam, applyToInstanceProperty, applyToInstanceMethod} = this;
-
     if (typeof descriptor === 'number') {
-      return applyToInstanceMethodParam(target, name as keyof P, descriptor);
+      return this.#applyToInstanceMethodParam(target, name as keyof P, descriptor);
     } else if (typeof descriptor === 'object') {
-      return applyToInstanceMethod(target, name, descriptor);
+      return this.#applyToInstanceMethod(target, name, descriptor);
     } else {
-      return applyToInstanceProperty(target, name);
+      return this.#applyToInstanceProperty(target, name);
     }
   }
 
@@ -200,13 +197,12 @@ export class DecoratorBuilder {
     name: keyof C,
     descriptor?: number | TypedPropertyDescriptor<F>,
   ) {
-    const {applyToStaticMethodParam, applyToStaticProperty, applyToStaticMethod} = this;
     if (typeof descriptor === 'number') {
-      return applyToStaticMethodParam(target, name, descriptor);
+      return this.#applyToStaticMethodParam(target, name, descriptor);
     } else if (typeof descriptor === 'object') {
-      return applyToStaticMethod(target, name, descriptor);
+      return this.#applyToStaticMethod(target, name, descriptor);
     } else {
-      return applyToStaticProperty(target, name);
+      return this.#applyToStaticProperty(target, name);
     }
   }
 }
