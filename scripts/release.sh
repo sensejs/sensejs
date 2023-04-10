644f8884d9a13ddddd757617f40e7e99032cf595
LAN Xingcan <thynson@live.com>
2023-04-11 01:02:10 +0800

    chore: force to provide a dist tag when running release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,2 +1,14 @@
-#!/bin/sh
-pnpm -r publish $@
+#!/bin/bash
+
+case "$1" in
+"alpha" | "beta" | "rc" | "latest")
+  pnpm -r publish --tag "$1"
+  ;;
+*)
+  echo "Usage: $0 <alpha|beta|rc|latest>"
+esac
+
+
+
+
+
